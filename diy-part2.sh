ab5971c21b1b1ef50e742439d9d7532aa84f8b9c
laozhaohaha <72434116+laozhaohaha@users.noreply.github.com>
2023-09-26 07:24:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@
 sed -i 's/192.168.1.1/192.168.1.111/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 #rm package/lean/luci-theme-argon -rf
